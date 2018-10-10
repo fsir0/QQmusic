b7d3855d3f07b4a12881eab6e04e18262a36b447
Mr.Fu <896787174@qq.com>
2018-10-10 23:54:22 +0800

    [+]add some structle

---
+++ build/js/pro.js
@@ -0,0 +1,71 @@
+// 进度条
+(function($, root) {
+    // 渲染时间 duration
+    var $scope = $(document.body);
+    // 用函数内的全局变量来保存：
+    // 当前歌曲总时长
+    // 动画驱动器
+    // 开始时间
+    // 当前停止时的百分比
+    var curDuration,
+        frameId,
+        startTime,
+        lastePercent = 0;
+    function renderAllTime(time) {
+        curDuration = time;
+        time = formatTime(time);
+        // 每次renderAllTime的时候将lastepercent清零
+        lastepercent = 0;
+        $scope.find('.all-time').html(time);
+    }
+    function formatTime(time) {
+        var min = Math.floor(time / 60);
+        var sec = time % 60;
+        // 对数据进行处理，小于10则在数字前加0
+        min = min < 10 ? '0' + min : min;
+        sec = sec < 10 ? '0' + sec : sec;
+        return min + ':' + sec;
+    }
+    // 用requestAnimationFrame记录播放事件
+    function start() {
+        startTime = new Date().getTime();
+        // requestAnimationFrame根据当前浏览器的驱动事件来触发事件
+        function frame() {
+            var curTime = new Date().getTime();
+            var percent = lastePercent + (curTime - startTime) / (curDuration * 1000);
+            console.log(percent);
+            // 判断当前时间不能超过总时长，超过则不执行更新时间了，并且不再循环此函数
+            if(percent < 1) {
+                // 类似于setTimeout
+                frameId = requestAnimationFrame(frame);
+                update(percent);
+            }
+        }
+        frame();
+    }
+    // 停止requestAnimationFrame
+    function stop() {
+        cancelAnimationFrame(frameId);
+        var stopTime = new Date().getTime();
+        // 让百分比进行累计，否则每次都会以上一次暂停重新计时。因此采用+=
+        lastePercent += (stopTime - startTime) / (curDuration * 1000);
+    }
+    // 实时更新进度条和开始时间
+    function update(per) {
+        // 计算出目前是当前这首歌的多少秒
+        var curTime = curDuration * per;
+        // 格式化时间
+        curTime = formatTime(Math.round(curTime));
+        $scope.find('.cur-time').html(curTime);
+        // 根据当前百分比渲染进度条
+        $scope.find('.pro-top').css({
+            transform: 'translateX(' + (per - 1) * 100 + '%)'
+        })
+    }
+    root.pro = {
+        renderAllTime,
+        update,
+        start,
+        stop
+    }
+})(window.Zepto, window.player || (window.player = {}))
--- src/js/index.js
+++ src/js/index.js
@@ -9,22 +9,63 @@ function bindEvent() {
         audio.getAudio(songList[index].audio);
         if(audio == "play") {
             audio.play();
+            root.pro.start();
         }
+        root.pro.renderAllTime(songList[index].duration);
+        root.render(songList[index]);
+        // 给上一首绑定事件
     }).on("click", ".prev-btn", function() {
         // 调用封装controlManager的判断当前index值
         index = controlManager.prev();
-        root.render(songList[index]);
         $scope.trigger("play:change", index);
+        // 给下一首绑定事件
     }).on("click", ".next-btn", function() {
         index = controlManager.next();
-        root.render(songList[index]);
         $scope.trigger("play:change", index);
+        // 给暂停和播放绑定事件
     }).on("click", ".play-btn", function() {
         if(audio.status == "play") {
             audio.pause();
+            root.pro.stop();
         } else {
-            audio.play()
+            audio.play();
+            root.pro.start();
         }
+    });
+    bindTouch();
+}
+// 进度条拖拽的实现
+function bindTouch() {
+    var $sliderBottom = $scope.find('.pro-wrapper');
+    var $slider = $scope.find('.pro-top');
+    var offset = $scope.find('.pro-bottom').offset();
+    var left = offset.left;
+    var width = offset.width;
+    $sliderBottom.on('touchend', function(e) {
+        var x = e.changedTouches[0].clientX;
+        var per = (x - left) / width;
+        // 点击时判断百分比大于1或则小于0时失效
+        if(per <= 0 || per >= 1) {
+            return;
+        }
+        root.pro.update(per);
+    });
+    $slider.on('touchstart', function() {
+
+    }).on('touchmove', function(e) {
+        // console.log(e) // 找到e中的clientX
+        var x = e.changedTouches[0].clientX;
+        var per = (x - left) / width;
+        // per在0~1实现拖拽,在之外进行置位处理
+        if(per > 0 && per < 1) {
+            root.pro.update(per);
+        } else if( per <= 0) {
+            root.pro.update(0);
+        } else {
+            root.pro.update(0.9999);
+        }
+    }).on('touchend', function() {
+        
     })
 }
 function getData(url) {
