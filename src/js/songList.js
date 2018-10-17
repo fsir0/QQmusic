655ba217904a1f2b1de2a608529d298b80fe41bb
Mr.Fu <896787174@qq.com>
2018-10-17 22:47:44 +0800

    fix song list

--- src/js/songList.js
+++ src/js/songList.js
@@ -1,12 +1,23 @@
 (function($, root) {
     var $scope = $(document.body);
     var $mark = $scope.find('.mark');
-    function listControl() {
-
+    var $songList = $scope.find('.song-list');
+    // Zepto中没有fadeIn方法，自己定义一个MyfadeIn方法,仅在此js内使用
+    function MyfadeIn($dom, item, value) {
+        $dom.css('display', 'block');
+        setTimeout(() => $dom.css(item, value), 0);
+    };
+    function show() {
+        MyfadeIn($mark, 'opacity', '0.8');
+        // MyfadeIn($songList, 'bottom', '0');
+    };
+    function hide() {
+        // $songList.css('bottom', '-200px');
+        $mark.css('opacity', '0');
+        // setTimeout(() => $mark.css('display', 'none'), 10);
     }
-    listControl.prototype = {
-        show: function() {
-            $mark.animate({display: 'block', opacity: '0.8'}, 400);
-        }
+    root.listControl = {
+        show,
+        hide
     }
 })(window.Zepto, window.player || (window.player = {}))
